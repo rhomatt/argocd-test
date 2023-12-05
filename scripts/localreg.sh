c2f757b7bee7593cbd294dd76aba23fade7d9554
Matthew Rho <rhomatt@protonmail.com>
2023-12-05 18:54:00 -0500

    auto remove reg

--- scripts/localreg.sh
+++ scripts/localreg.sh
@@ -2,7 +2,8 @@
 
 cd $(dirname $0)
 
-docker run -d -p 5000:5000 --name registry registry:2 || echo registry already running
+docker rmi -f registry:2
+docker run -d -p 5000:5000 --name registry registry:2
 
 cd ../images/
 docker build -t localhost:5000/alpine .
